1aaf700048525222a52eb8f5493697fbb384d36b
demokedes <rok.rogelj@gmail.com>
2017-03-28 13:49:41 +0200

    Update before-install.bat

--- before-install.bat
+++ before-install.bat
@@ -1,7 +1,6 @@
 REM Install Internet Information Server (IIS). 
 net stop w3svc /Y
 powershell.exe -command "Start-Sleep -Milliseconds 5000">nul
-Msg * "insert your message here"  Testiranje
 net start w3svc /Y
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
