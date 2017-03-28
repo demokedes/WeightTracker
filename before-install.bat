0f02dd410c5668894377c32e66bb50130c810e0f
demokedes <rok.rogelj@gmail.com>
2017-03-28 13:22:54 +0200

    Halt

--- before-install.bat
+++ before-install.bat
@@ -1,4 +1,10 @@
 REM Install Internet Information Server (IIS). 
-Msg * "insert your message here"
+::Msg * "insert your message here"  Testiranje
+:: zstop IIS server
+net stop w3svc /Y
+::Waiting for other services to start
+timeout /T 10 /NOBREAK
+::starting IIS
+net start w3svc /Y
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
