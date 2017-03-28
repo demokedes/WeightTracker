b95e21e9cbae6a05a9ebdf28466925f24326f1f7
demokedes <rok.rogelj@gmail.com>
2017-03-28 13:04:13 +0200

    Dodajanje message-a

--- before-install.bat
+++ before-install.bat
@@ -1,3 +1,4 @@
 REM Install Internet Information Server (IIS). 
+Msg * "insert your message here"
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
