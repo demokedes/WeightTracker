dfb4a68d932b2e7bd71d08c26fb5436960127032
demokedes <rok.rogelj@gmail.com>
2017-03-28 13:47:49 +0200

    Update before-install.bat

--- before-install.bat
+++ before-install.bat
@@ -1,6 +1,6 @@
 REM Install Internet Information Server (IIS). 
 net stop w3svc /Y
-timeout /T 10 /NOBREAK >NUL
+powershell.exe -command "Start-Sleep -Milliseconds 5000">nul
 Msg * "insert your message here"  Testiranje
 net start w3svc /Y
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
