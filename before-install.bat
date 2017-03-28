9b7325a5f49e87812fdafea1664df957181b7d27
demokedes <rok.rogelj@gmail.com>
2017-03-28 13:33:47 +0200

    Update before-install.bat

--- before-install.bat
+++ before-install.bat
@@ -1,9 +1,9 @@
 REM Install Internet Information Server (IIS). 
-::Msg * "insert your message here"  Testiranje
-:: zstop IIS server
+:: stop IIS server
 net stop w3svc /Y
 ::Waiting for other services to start
 timeout /T 10 /NOBREAK
+Msg * "insert your message here"  Testiranje
 ::starting IIS
 net start w3svc /Y
 c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
